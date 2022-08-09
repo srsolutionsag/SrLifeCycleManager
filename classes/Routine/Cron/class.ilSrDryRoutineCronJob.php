0a11539270de5f5d5895561de48ff48757fb3496
Fabian Schmid <fabian@sr.solutions>
2022-08-09 18:16:26 +0200

    [FIX] usage of Interface

--- classes/Routine/Cron/class.ilSrDryRoutineCronJob.php
+++ classes/Routine/Cron/class.ilSrDryRoutineCronJob.php
@@ -2,7 +2,7 @@
 
 /* Copyright (c) 2022 Thibeau Fuhrer <thibeau@sr.solutions> Extended GPL, see docs/LICENSE */
 
-use srag\Plugins\SrLifeCycleManager\Notification\Reminder\Reminder\IReminder;
+use srag\Plugins\SrLifeCycleManager\Notification\Reminder\IReminder;
 use srag\Plugins\SrLifeCycleManager\Routine\IRoutine;
 
 /**
