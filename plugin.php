9998413df7e4fa95668a07a2446ceb4ae250e0e5
Fabian Schmid <fabian@sr.solutions>
2024-11-21 16:53:32 +0100

    [FEATURE] ILIAS Versions

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
 
 $id = "srlcm";
-$version = "2.1.1";
-$ilias_min_version = "8.000";
-$ilias_max_version = "8.999";
+$version = "3.0.0";
+$ilias_min_version = "9.000";
+$ilias_max_version = ").999";
 
 $responsible = "sr solutions ag";
 $responsible_mail = "support@sr.solutions";
